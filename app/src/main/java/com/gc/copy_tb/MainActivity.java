86d25542e08fe468c15f32d55d2bb7aca522bea6
gc <cguo@sparkpad.com>
2020-07-23 15:10:44 +0800

    add utilcodex dependencies

--- app/src/main/java/com/gc/copy_tb/MainActivity.java
+++ app/src/main/java/com/gc/copy_tb/MainActivity.java
@@ -1,17 +1,17 @@
 package com.gc.copy_tb;
 
-import android.os.Handler;
-import android.os.Message;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.widget.Toast;
+
+import com.blankj.utilcode.util.ToastUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String message = "";
+    private long clickTime = 0; //记录第一次点击的时间
+
     private String TAG = "MainActivity";
 
     @Override
@@ -19,27 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    //展示消息
-    public void showMessage(String meg) {
-        if (message.equals(meg)) {
-            return;
-        }
-        message = meg;
-        Log.d(TAG, "弹出内容:" + message);
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-
-    private static boolean isExit = false;
-
-    Handler mHandler = new Handler() {
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            isExit = false;
-        }
-    };
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
@@ -51,15 +30,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void exit() {
-        if (!isExit) {
-            isExit = true;
-            Toast.makeText(getApplicationContext(), "再按一次退出程序",
-                    Toast.LENGTH_SHORT).show();
-            // 利用handler延迟发送更改状态信息
-            mHandler.sendEmptyMessageDelayed(0, 2000);
+        if ((System.currentTimeMillis() - clickTime) > 2000) {
+            ToastUtils.showShort("再按一次退出");
+            clickTime = System.currentTimeMillis();
         } else {
             finish();
-            System.exit(0);
+            ToastUtils.cancel();
         }
     }
 
